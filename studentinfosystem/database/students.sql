29086097e431d5e02f98c4c82d17631940b9ed2c
Joyin <joyinlaskar@gmail.com>
2022-03-16 16:41:51 +0530

    database

--- studentinfosystem/database/students.sql
+++ studentinfosystem/database/students.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2022 at 11:26 AM
+-- Generation Time: Mar 16, 2022 at 12:10 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -44,19 +44,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `email`, `password`, `studentno`, `firstname`, `lastname`, `course`, `yrlevel`, `date_joined`) VALUES
-(15, 'testuser', 'testuser', 1, 'Test', 'User', 'BSIT', '1st year', '2017-05-28'),
-(16, 'johndoe', 'johndoe', 403281, 'John', 'Doe', 'BSBA', '3rd year', '2017-05-28'),
-(17, 'harryden', 'harry', 12345, 'Harry', 'Den', 'BSIT', '4th year', '2018-12-24'),
-(18, 'admin', 'admin', 2147483647, 'Joyin Mahmmad Aslam', 'Laskar', 'BSOA', '4th year', '2022-03-14'),
-(19, 'joyinlas', 'joyinlas', 2147483647, 'joyin', 'Laskar', 'BSBA', '1st year', '2022-03-14'),
-(20, 'admin33', 'admin', 2147483647, 'jnjsnc', 'karim', 'BSBA', '1st year', '2022-03-14'),
-(21, 'admin44', 'admin', 3333333, 'sdfsdf', 'afsadfsfasdf', 'BSBA', '1st year', '2022-03-14'),
-(22, 'jfvdfjv', 'kkkk', 23423432, 'joyin', 'laskar', 'BSBA', '1st year', '2022-03-14'),
-(23, 'adminddd', 'adminddd', 27376353, 'rock', 'the', 'BSBA', '1st year', '2022-03-14'),
-(24, 'adminsdfds', 'joyin', 2147483647, 'dfgfdgsdf', 'sgfdgfdg', 'BSBA', '1st year', '2022-03-14'),
-(25, 'dfdf', 'joyin', 63542424, 'Joyin Mahmmad Aslam', 'Laskar', 'BSBA', '1st year', '2022-03-14'),
-(26, 'hdfhbfhdbv', 'joyin', 0, 'jdnfdsjf', 'joyin.las.2000@gmail.com', 'BSBA', '1st year', '2022-03-14'),
-(27, 'joyin.las.2000@gmail.com', 'joyin', 2147483647, 'rajiv', 'Laskar', 'BSBA', '1st year', '2022-03-14');
+(44, 'joyin.las.2000@gmail.com', 'joyin', 2147483647, 'Joyin Mahmmad Aslam', 'Laskar', 'BSBA', '1st year', '2022-03-16');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +54,8 @@ INSERT INTO `students` (`id`, `email`, `password`, `studentno`, `firstname`, `la
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `studentno` (`studentno`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -76,7 +65,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
